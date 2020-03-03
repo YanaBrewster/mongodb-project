e3604ff4a78e0013795bf261b50a914e02990751
YanaBrewster <yana.brewster@gmail.com>
2020-03-03 15:34:50 +1300

    removed key

--- index.js
+++ index.js
@@ -36,14 +36,14 @@ app.get('/allProducts', (req,res)=>{
   res.json(product)
 });
 
-app.get('/product/:id', (req,res) =>{
-const idParam = req .params.id;
-  for (let i = 0; i < product.length; i++){
-    if (idParam === product[i].id) {
-      res.json(product[i].id);
-    } 
-  }
-})
+// app.get('/products/p=:id', (req,res) =>{
+// const idParam = req .params.id;
+//   for (let i = 0; i < product.length; i++){
+//     if (idParam === product[i].id) {
+//       res.json(product[i].id);
+//     }
+//   }
+// })
 
 // keep this at the end so errors can be seen
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
